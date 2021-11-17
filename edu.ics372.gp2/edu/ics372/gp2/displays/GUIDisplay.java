4c8a0384ad8eba6c5eff52fff9e7016cfb64a01a
Dilli Khatiwoda <dilli.khatiwoda@yahoo.com>
2021-11-17 11:09:05 -0600

    Updated GUIDisplay class

--- edu.ics372.gp2/edu/ics372/gp2/displays/GUIDisplay.java
+++ edu.ics372.gp2/edu/ics372/gp2/displays/GUIDisplay.java
@@ -3,10 +3,55 @@
  */
 package edu.ics372.gp2.displays;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
- * @author dilli
+ * @author Dilli
  *
  */
-public class GUIDisplay {
+public class GUIDisplay extends Application implements VideoPlayerDisplay {
+
+	@Override
+	public void showTimeRemaining(int time) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showTurnOn() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showTurnOff() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showPaused() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showPlaying() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showStopped() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
 
 }
