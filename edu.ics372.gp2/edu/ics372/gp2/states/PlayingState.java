f1993d4b687d3d41dd02dfcb1ff63b4caa9bac82
leoflewis <fleonardolewis@gmail.com>
2021-11-26 11:48:44 -0600

    added playing state, added super class methods

---
+++ edu.ics372.gp2/edu/ics372/gp2/states/PlayingState.java
@@ -0,0 +1,74 @@
+package edu.ics372.gp2.states;
+
+import edu.ics372.gp2.timer.Notifiable;
+
+public class PlayingState extends VideoPlayerState implements Notifiable{
+	private static PlayingState instance;
+	
+	/**
+	 * Singleton for playing state
+	 */
+	private PlayingState() {
+		
+	}
+	
+	/**
+	 * Creates lone instance of playing state
+	 * @return
+	 */
+	public static PlayingState getInstance() {
+		if (instance == null) {
+			instance = new PlayingState();
+		}
+		return instance;
+	}
+	
+	//TODO add other events
+	
+	/**
+	 * Process play request
+	 */
+	@Override
+	public void onPlayRequest() {
+		//update timer
+		//display time
+	}
+	
+	/**
+	 * Process timer tick
+	 */
+	@Override
+	public void OnTimerTick(int timerValue) {
+		// TODO Auto-generated method stub
+		// show time left
+	}
+
+	/**
+	 * Process the video ending
+	 */
+	@Override
+	public void onVideoRunsOut() {
+		// TODO Auto-generated method stub
+		// show 0 time left
+		// change to idle state
+	}
+
+	/**
+	 * Process leaving playing state
+	 */
+	@Override
+	public void leave() {
+		//set clock to zero and null
+		//change video player context to not playing
+	}
+
+	/**
+	 * Enter playing state
+	 */
+	@Override
+	public void enter() {
+		//create new timer
+		//show time of video
+	}
+
+}
--- edu.ics372.gp2/edu/ics372/gp2/states/VideoPlayerState.java
+++ edu.ics372.gp2/edu/ics372/gp2/states/VideoPlayerState.java
@@ -1,5 +1,68 @@
 package edu.ics372.gp2.states;
 
+/**
+ * Super class for all states
+ * @author leo
+ *
+ */
 public abstract class VideoPlayerState {
-
+	
+	/**
+	 * leave current state
+	 */
+	public abstract void leave();
+	
+	/**
+	 * enter current state
+	 */
+	public abstract void enter();
+	
+	/**
+	 * Process play request
+	 */
+	public void onPlayRequest() {
+		
+	}
+	
+	/**
+	 * Process pause
+	 */
+	public void onPauseRequest() {
+		
+	}
+	
+	/**
+	 * Process stop
+	 */
+	public void onStopRequest() {
+		
+	}
+	
+	/**
+	 * Process rewind
+	 */
+	public void onRewindRequest() {
+		
+	}
+	
+	/**
+	 * Process fast forward
+	 */
+	public void onFastFowardRequest() {
+		
+	}
+	
+	/**
+	 * Process off
+	 */
+	public void onOffRequest() {
+		
+	}
+	
+	/**
+	 * Process selection
+	 */
+	public void onSelectRequest() {
+		
+	}
 }
