1b807022400cfe21c3722c23553377cfb3cac1a3
andy <andy@10.0.0.114>
2021-11-27 04:22:32 -0600

    added documentation and update on some method

--- edu.ics372.gp2/edu/ics372/gp2/states/UnselectedState.java
+++ edu.ics372.gp2/edu/ics372/gp2/states/UnselectedState.java
@@ -1,12 +1,21 @@
 package edu.ics372.gp2.states;
 
+import edu.ics372.gp2.entities.Show;
+
 public class UnselectedState extends VideoPlayerState{
-private static UnselectedState instance;
+	private static UnselectedState instance;
 	
+	/**
+	 * private constructor for singleton pattern
+	 */
 	private UnselectedState() {
 		
 	}
 	
+	/**
+	 * for singleton pattern
+	 * @return UnselectedState object
+	 */
 	public static UnselectedState getInstance() {
 		if(instance == null) {
 			instance = new UnselectedState();
@@ -14,23 +23,38 @@ private static UnselectedState instance;
 		return instance;
 	}
 
+	/**
+	 * process Off request.
+	 */
+	public void onOffRequest() {
+		VideoPlayerContext.getInstance().changeState(OffState.getInstance());
+	}
+
+	/**
+	 * process select show request
+	 * @param show
+	 */
+	public void onSelectRequest(Show show) {
+		VideoPlayerContext.getInstance().setShow(show);
+		VideoPlayerContext.getInstance().changeState(SelectedState.getInstance());
+	}
+	
+	/**
+	 * initialize the State.
+	 */
 	@Override
 	public void enter() {
 		VideoPlayerContext.getInstance().showUnselected();
 	}
 
+	/**
+	 * Uninitialized the state.
+	 * @param newState
+	 */
 	@Override
 	public void leave(VideoPlayerState newState) {
 		if(newState.equals(OffState.getInstance())) {
 			VideoPlayerContext.getInstance().showSelectOff();
 		}
-	}
-	
-	public void onOffRequest() {
-		VideoPlayerContext.getInstance().changeState(OffState.getInstance());
-	}
-
-	public void onSelectRequest() {
-		VideoPlayerContext.getInstance().changeState(SelectedState.getInstance());
 	}
 }
