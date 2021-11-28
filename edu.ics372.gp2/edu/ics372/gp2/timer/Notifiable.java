2badc2fcb0b1b96f100bbb149310295f3d89c5f3
axw1101 <austin.x.wng@gmail.com>
2021-11-27 22:00:33 -0600

    merged Notifiable, Timer, and Clock classes (#14)

--- edu.ics372.gp2/edu/ics372/gp2/timer/Notifiable.java
+++ edu.ics372.gp2/edu/ics372/gp2/timer/Notifiable.java
@@ -1,15 +1,22 @@
 package edu.ics372.gp2.timer;
 
+/**
+ * An entity that can be notified of timing events
+ * 
+ *
+ */
 public interface Notifiable {
-
+	
 	/**
-	 * Process timer ticks
-	 */
+	   * Process timer ticks
+	*/
 	public void OnTimerTick(int timerValue);
-
+	
 	/**
 	 * Process timer runs out event
 	 */
 	public void onTimerRunsOut();
+	
+	public void onVideoRunsOut();
 
 }
