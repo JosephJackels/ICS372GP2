c9111a59e75794bed641a14719619b5afbae6776
leoflewis <fleonardolewis@gmail.com>
2021-11-29 16:15:03 -0600

    show ended 10 sec count down, can play again

--- edu.ics372.gp2/edu/ics372/gp2/displays/GUIDisplay.java
+++ edu.ics372.gp2/edu/ics372/gp2/displays/GUIDisplay.java
@@ -131,7 +131,7 @@ public class GUIDisplay extends Application implements VideoPlayerDisplay {
 	 */
 	@Override
 	public void showStopped() {
-		videoPlayerStatus.setText("Video Status: Stopped, show ended\n");
+		videoPlayerStatus.setText("Video Status: show ended\n");
 		infoText.setText("");
 		timerValue.setText("");
 	}
