1720bc6ffff9465c789621bb3b2fef0c299e2c23
leoflewis <53980438+leoflewis@users.noreply.github.com>
2021-12-02 15:31:47 -0600

    Update OffButton.java

--- edu.ics372.gp2/edu/ics372/gp2/buttons/OffButton.java
+++ edu.ics372.gp2/edu/ics372/gp2/buttons/OffButton.java
@@ -4,8 +4,15 @@ import edu.ics372.gp2.states.VideoPlayerContext;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
+/**
+ * The button for off request
+ */
 public class OffButton extends GUIButton implements EventHandler<ActionEvent> {
-
+	/**
+	 * The button for turning the video player off
+	 * 
+	 * @param string
+	 */
 	public OffButton(String string) {
 		super(string);
 	}
