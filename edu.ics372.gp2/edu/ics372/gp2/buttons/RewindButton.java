33e78f07c6de7e0e06c249e0d99aa1dfabcae653
leoflewis <53980438+leoflewis@users.noreply.github.com>
2021-12-02 15:33:07 -0600

    Update RewindButton.java

--- edu.ics372.gp2/edu/ics372/gp2/buttons/RewindButton.java
+++ edu.ics372.gp2/edu/ics372/gp2/buttons/RewindButton.java
@@ -4,8 +4,16 @@ import edu.ics372.gp2.states.VideoPlayerContext;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
+/**
+ * The button for rewind request
+ */
 public class RewindButton extends GUIButton implements EventHandler<ActionEvent> {
 
+	/**
+	 * The button for rewinding
+	 * 
+	 * @param string
+	 */
 	public RewindButton(String string) {
 		super(string);
 	}
