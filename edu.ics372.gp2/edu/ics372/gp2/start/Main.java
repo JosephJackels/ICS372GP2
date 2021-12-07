ee145d7f053927dc84108238b5242dbcb1f11ddd
Dilli Khatiwoda <dilli.khatiwoda@yahoo.com>
2021-12-07 14:39:33 -0600

    Documented .gp2.start package

--- edu.ics372.gp2/edu/ics372/gp2/start/Main.java
+++ edu.ics372.gp2/edu/ics372/gp2/start/Main.java
@@ -3,6 +3,12 @@ package edu.ics372.gp2.start;
 import edu.ics372.gp2.displays.GUIDisplay;
 import javafx.application.Application;
 
+/**
+ * This class starts the program.
+ * 
+ * @author Dilli, Joseph, Andy , Leo, Austin
+ *
+ */
 public class Main {
 	public static void main(String[] args) {
 		new Thread() {
